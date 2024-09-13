ab5fddc894cf4caf5369b96ab0e892e85eabe154
pesadaum <alefdeoliveirasantos@gmail.com>
2024-09-12 21:15:24 -0300

    even more enhancements on TB

--- src/asic/_top/top_tb.sv
+++ src/asic/_top/top_tb.sv
@@ -4,9 +4,9 @@ module top_tb ();
 localparam BUS_WIDTH = 10;
 // -- module: Q measurement
 localparam WTD_BUS_WIDTH = 3 ;
-localparam Q_PER_PULSE   = 10;
+localparam Q_PER_PULSE   = 1;
 // -- module: control
-localparam TOL = Q_PER_PULSE - 5;
+localparam TOL = 5;
 // -- module: instability determination
 localparam I_REF_DELTA_INSTB = 10;
 localparam DELTA_Q_INSTB     = 50;
@@ -45,7 +45,7 @@ resonant_sys #(
   .BUS_WIDTH     (BUS_WIDTH     ),
   .PULSE_DURATION(PULSE_DURATION),
   .Q_PER_PULSE   (Q_PER_PULSE   ),
-  .INCLUDE_Q_DROP   (INCLUDE_Q_DROP   )
+  .INCLUDE_Q_DROP(INCLUDE_Q_DROP)
 ) resonant_sys_inst (
   .i_ref       (i_ref_w     ),
   .start       (start       ),
@@ -53,14 +53,13 @@ resonant_sys #(
 );
 
 // Strobe for pulses ended debug signal
-always @(q_serialized) begin
+always @(q_serialized) begin : variables_strobe
   pulses_ended = resonant_sys_inst.pulses_ended;
 end
 
 
 integer vars_file;
-
-initial begin
+initial begin : variables_log
   // q_serialized = 0;
   start        = 0;
   clk          = 0;
@@ -70,8 +69,8 @@ initial begin
 
   // i_ref_w    = 2**BUS_WIDTH-1;
   vars_file = $fopen("vars.txt", "w");
-  $fwrite(vars_file, "time clk rst start enable i_ref q_measured ready q_desired\n");
-  $fmonitor(vars_file, $time,, clk,, rst,, start,, enable,, i_ref_w,, top_inst.q_measured_w,, top_inst.ready_w,, q_desired);
+  $fwrite(vars_file, "time clk rst start enable i_ref q_measured ready q_desired converged\n");
+  $fmonitor(vars_file, $time,, clk,, rst,, start,, enable,, i_ref_w,, top_inst.q_measured_w,, top_inst.ready_w,, q_desired,, top_inst.q_control_inst.converged);
 end
 
 
@@ -79,22 +78,48 @@ end
 always
   #1 clk = ~clk;
 
-integer max_timeout = 10_000; // ps
+int unsigned values_sweep[18]     ; // Array size 10, change it as needed
+int unsigned step             = 10;
+int unsigned lower_b; 
+int unsigned upper_b;
 
+initial begin: random_q_seek_generation
+  foreach (values_sweep[i]) begin
+
+    lower_b = step + (step * (i+1));
+    upper_b = step + (2 * step * (i+1));
+    
+    values_sweep[i] = $urandom_range(lower_b, upper_b);
+    $display("Generated random value = %d", values_sweep[i]);
+  end
+end
+
+int max_timeout; // ps
 initial begin
 
 
   #5 rst = 1;  #5 rst = 0;
   #5 start = 1; enable = 1;
 
-  q_desired = 301;
-
-  while (!top_inst.q_control_inst.converged) begin
-    #1 ;
-    max_timeout--;
-    if(max_timeout == 0)
-      $stop();
+  
+  foreach (values_sweep[i]) begin : q_desired_sweep
+    max_timeout = 5_0000;
+    q_desired = values_sweep[i];
+    while (!top_inst.q_control_inst.converged) begin
+      #1 ;
+      max_timeout--;
+      if(max_timeout == 0)
+        break;
+    end
+    #5 rst = 1;  #5 rst = 0;
   end
+
+  // while (!top_inst.q_control_inst.converged) begin
+  //   #1 ;
+  //   max_timeout--;
+  //   if(max_timeout == 0)
+  //     $stop();
+  // end
   # 100;
   $fclose(vars_file);
   $stop();
